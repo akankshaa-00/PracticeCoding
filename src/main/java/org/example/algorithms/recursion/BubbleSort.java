4236477c0507d2b9fc8c170f63c93ccf7bbee343
Akshat Tandon <tandonakshat102@gmail.com>
2026-03-05 14:24:11 +0530

    bubblesort with recursion

--- src/main/java/org/example/algorithms/recursion/BubbleSort.java
+++ src/main/java/org/example/algorithms/recursion/BubbleSort.java
@@ -1,4 +1,41 @@
 package org.example.algorithms.recursion;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        int[] arr={2,8,1,5,7};
+        sort( arr, 0,arr.length-1,0);
+    }
+
+    public static void sort(int[] arr,int index,int targetindex,int largestelement)
+    {
+        if(targetindex==0)
+        {
+            System.out.println(Arrays.toString(arr));
+        }
+
+        if(index==targetindex)
+        {
+            swap(arr,largestelement,targetindex);
+             sort(arr,0,targetindex-1,0);
+        } else if(index<targetindex)
+        {
+            if(arr[index]>arr[largestelement]) {
+                largestelement = index;
+            }
+
+             sort(arr,index+1,targetindex,largestelement);
+        }
+
+    }
+
+    private static void swap(int[] arr, int largestin, int targetindex) {
+        int temp=arr[largestin];
+        arr[largestin]=arr[targetindex];
+        arr[targetindex]=temp;
+    }
 }
